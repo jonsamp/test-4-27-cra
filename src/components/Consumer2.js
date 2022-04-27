13bd135579e685130faa331090c46d2123ff11ad
John Pham <johnphammail@gmail.com>
2022-04-27 16:16:47 -0700

    Add demo

--- src/components/Consumer2.js
+++ src/components/Consumer2.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import useToggle from '../hooks/useToggle';
+import React from "react";
 
-export function Consumer2() {
-  const [toggleValue, toggle] = useToggle(false);
-
+export function Consumer2(toggleValue, toggle) {
   return (
-    <div style={{ backgroundColor: 'lightcyan', padding: '1rem' }}>
-      <p>Consumer2: {toggleValue ? 'on' : 'off'}</p>
+    <div style={{ backgroundColor: "lightcyan", padding: "1rem" }}>
+      <p>Consumer2: {toggleValue ? "on" : "off"}</p>
       <button onClick={toggle}>Toggle</button>
     </div>
   );
